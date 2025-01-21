d0bf860adc5a8e74996d39e8a54a210828303a62
Athens Dubey <athensdubey@gmail.com>
2025-01-21 20:44:02 +0530

    s

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
+import cors from "cors";
 const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
-const cors = require("cors");
 require('dotenv').config(); 
 
 
@@ -35,33 +35,15 @@ app.use(express.json());
 app.use(
   cors({
     origin: process.env.CLIENT_URL,
-    methods: ["GET", "POST", "DELETE", "PUT"],
-    // allowedHeaders: [
-    //   "Content-Type",
-    //   "Authorization",
-    //   "Cache-Control",
-    //   "Expires",
-    //   "Pragma",
-    // ],
     credentials: true,
   })
 );
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL);
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 
 mongoose.connect(
   process.env.MONGO_URL
 ).then(console.log("MongoDb Connected"))
 .catch((error) => console.log(error))
-
-
-
-
 
 
 app.use("/api/auth", authRouter);
